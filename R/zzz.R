0d6cd0f14d442361e6256e38cb863cd02f5f9208
ShixiangWang <wangshx@shanghaitech.edu.cn>
2018-11-22 10:51:41 +0800

    add start up message

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+.onAttach = function(libname, pkgname) {
+    version = packageDescription(pkgname, fields = "Version")
+
+    msg = paste0("=========================================================================
+", pkgname, " version ", version, "
+Github page: https://github.com/ShixiangWang/VSHunter
+Documentation: https://github.com/ShixiangWang/VSHunter
+If you use it in published research, please cite:
+Macintyre, Geoff, et al. \"Copy number signatures and mutational
+    processes in ovarian carcinoma.\" Nature genetics 50.9 (2018): 1262.
+Wang, Shixiang, et al. \"APOBEC3B and APOBEC mutational signature
+    as potential predictive markers for immunotherapy
+    response in non-small cell lung cancer.\" Oncogene (2018).
+=========================================================================
+                 ")
+    packageStartupMessage(msg)
+}
